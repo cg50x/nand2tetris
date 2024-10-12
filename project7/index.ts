f95bc2ae1a09c025a0a98d7d33d3bf1521f41ba5
Chris Guevara <chrismguevara@gmail.com>
2024-10-12 19:27:34 +0900

    project 7: Finished; passed all tests

--- project7/index.ts
+++ project7/index.ts
@@ -1,30 +1,100 @@
 import type { BunFile, FileSink } from "bun";
+import {
+  writeAdd,
+  writeAnd,
+  writeEq,
+  writeGt,
+  writeLt,
+  writeNeg,
+  writeNot,
+  writeOr,
+  writePopArgument,
+  writePopLocal,
+  writePopPointer0,
+  writePopPointer1,
+  writePopStatic,
+  writePopTemp,
+  writePopThat,
+  writePopThis,
+  writePushArgument,
+  writePushConstant,
+  writePushLocal,
+  writePushPointer0,
+  writePushPointer1,
+  writePushStatic,
+  writePushTemp,
+  writePushThat,
+  writePushThis,
+  writeSub,
+} from "./writer-utils";
 
 type CommandType =
-| 'C_ARITHMETIC'
-| 'C_PUSH'
-| 'C_POP'
-| 'C_LABEL'
-| 'C_GOTO'
-| 'C_IF'
-| 'C_FUNCTION'
-| 'C_RETURN'
-| 'C_CALL';
+  | "C_ARITHMETIC"
+  | "C_PUSH"
+  | "C_POP"
+  | "C_LABEL"
+  | "C_GOTO"
+  | "C_IF"
+  | "C_FUNCTION"
+  | "C_RETURN"
+  | "C_CALL";
+
+type ArithmeticCommand =
+  | "add"
+  | "sub"
+  | "neg"
+  | "eq"
+  | "gt"
+  | "lt"
+  | "and"
+  | "or"
+  | "not";
 
 const arithCommands = [
-  'add', 'sub', 'neg', 'eq', 'gt', 'lt', 'and', 'or', 'not'
+  "add",
+  "sub",
+  "neg",
+  "eq",
+  "gt",
+  "lt",
+  "and",
+  "or",
+  "not",
 ];
+
+function isArithmeticCommand(command?: string): command is ArithmeticCommand {
+  return !!command && arithCommands.includes(command);
+}
+
+type MemorySegment =
+  | "local"
+  | "argument"
+  | "static"
+  | "constant"
+  | "this"
+  | "that"
+  | "pointer"
+  | "temp";
+const memorySegments = [
+  "local", "argument", "static", "constant", "this", "that", "pointer", "temp"
+];
+
+function isMemorySegment(segment?: string): segment is MemorySegment {
+  return !!segment && memorySegments.includes(segment);
+}
 
 class Parser {
   private streamReader: ReadableStreamDefaultReader<Uint8Array> | undefined;
-  private buffer: string = '';
+  private buffer: string = "";
   private textDecoder = new TextDecoder();
   private commandBuffer: string[] = [];
-  private currentCommand: {
-    commandType: CommandType;
-    arg1: string;
-    arg2: number;
-  } | undefined;
+  private currentCommand:
+    | {
+        commandType: CommandType;
+        arg1: string;
+        arg2: number;
+      }
+    | undefined;
   static async create(file: BunFile) {
     const newParser = new Parser();
     const newStream = await file.stream();
@@ -35,14 +105,14 @@ class Parser {
 
   public async hasMoreCommands(): Promise<boolean> {
     if (!this.streamReader) {
-      throw new Error('Reader not initialized');
+      throw new Error("Reader not initialized");
     }
     if (this.commandBuffer.length > 0) {
       return true;
     }
     let readerDone = false;
     const bufferHasNewline = () => {
-      return !(this.buffer.includes('\r\n') || this.buffer.includes('\n'));
+      return this.buffer.includes("\r\n") || this.buffer.includes("\n");
     };
     while (!readerDone && !bufferHasNewline()) {
       const { value: chunk, done } = await this.streamReader.read();
@@ -54,15 +124,15 @@ class Parser {
     }
     // If reader is done, and buffer is empty, we are done, no more commands
     if (this.buffer.length === 0 && readerDone) {
-      return false
+      return false;
     }
 
     // Collecting the lines except for the last chunk
     let lines = this.buffer.split(/\r?\n/);
-    this.buffer = lines.pop() ?? '';
+    this.buffer = lines.pop() ?? "";
     // Ignore all whitespace and comments
     lines = lines
-      .map((line) => line.replace(/\/\/.*/, ''))
+      .map((line) => line.replace(/\/\/.*/, ""))
       .map((line) => line.trim())
       .filter((line) => line.length > 0);
     // Assumption: The file is all valid commands
@@ -74,7 +144,7 @@ class Parser {
   public async advance(): Promise<void> {
     if (await this.hasMoreCommands()) {
       const nextCommand = this.commandBuffer.shift();
-      if (arithCommands.includes(nextCommand ?? '')) {
+      if (arithCommands.includes(nextCommand ?? "")) {
         this.currentCommand = {
           commandType: "C_ARITHMETIC",
           arg1: nextCommand as string,
@@ -89,7 +159,7 @@ class Parser {
         this.currentCommand = {
           commandType: "C_PUSH",
           arg1: segment,
-          arg2: parseInt(index)
+          arg2: parseInt(index),
         };
         return;
       }
@@ -100,7 +170,7 @@ class Parser {
         this.currentCommand = {
           commandType: "C_POP",
           arg1: segment,
-          arg2: parseInt(index)
+          arg2: parseInt(index),
         };
         return;
       }
@@ -123,6 +193,13 @@ class Parser {
 class CodeWriter {
   private fileWriter: FileSink | undefined;
 
+  /** Used to create identifier for eq jump labels */
+  private nextEqId = 0;
+  /** Used to create identifier for gt jump labels */
+  private nextGtId = 0;
+  /** Used to create identifier for lt jump labels */
+  private nextLtId = 0;
+
   static async create(file: BunFile) {
     const newCodeWriter = new CodeWriter();
     newCodeWriter.fileWriter = file.writer();
@@ -130,171 +207,131 @@ class CodeWriter {
   }
   private constructor() {}
 
-  public writeArithmetic(command: string): void {
-    // impl
-    /*
-    'add', = D+(AM)
-    'sub', = D-AM
-    'neg', = -D/-A
-    'eq', = Use JEQ
-    'gt', = Use JGT
-    'lt', = Use JLT
-    'and', = D&A (ends up either 0 or -1)
-    'or', = D|A
-    'not' = -D/-A
-
-    Compute field of the C-instruction
-    0
-    1
-    -1
-    D
-    A
-    !D
-    !A
-    -D
-    -A
-    D+1
-    A+1
-    D-1
-    A-1
-    D+A
-    D-A
-    A-D
-    D&A
-    D|A
-    */
+  public writeArithmetic(command: ArithmeticCommand): void {
+    let translation;
+    if (command === "add") {
+      translation = writeAdd();
+    } else if (command == "and") {
+      translation = writeAnd();
+    } else if (command == "eq") {
+      translation = writeEq({ id: `${this.nextEqId++}` });
+    } else if (command == "gt") {
+      translation = writeGt({ id: `${this.nextGtId++}` });
+    } else if (command == "lt") {
+      translation = writeLt({ id: `${this.nextLtId++}` });
+    } else if (command == "neg") {
+      translation = writeNeg();
+    } else if (command == "not") {
+      translation = writeNot();
+    } else if (command == "or") {
+      translation = writeOr();
+    } else if (command == "sub") {
+      translation = writeSub();
+    }
+    if (translation) {
+      this.fileWriter?.write(translation);
+    }
   }
 
-  public writePushPop(command: "C_PUSH" | "C_POP", segment: string, index: number): void {
-    /*
-      Eight memory segments:
-      - local (@LCL)
-      - argument (@ARG)
-      - static (use @Foo.i)
-      - constant (use @i)
-      - this (@THIS)
-      - that (@THAT)
-      - pointer (pointer 0 = @THIS, pointer 1 = @THAT)
-      - temp (Use @5 - @12 (0-7))
-    */
-    /*
-      push constant i
-      // RAM[SP] = i
-      @i
-      D=A
-      @SP
-      A=M
-      M=D
-      // SP++
-      @SP
-      M=M+1
-      
-      push local i
-      // addr = segmentPointer + i
-      @i
-      D=A
-      @LCL
-      D=D+A
-      // RAM[SP] = RAM[addr]
-      @SP
-      A=M
-      M=D
-      // SP++
-      @SP
-      M=M+1
-
-      pop local i
-      // addr = segmentPointer + i
-      @i
-      D=A
-      @LCL
-      D=A+D
-      @addr
-      M=D
-      // SP--
-      @SP
-      M=M-1
-      // RAM[addr] = RAM[SP]
-      A=M
-      D=M
-      @addr
-      M=D
-
-      push argument i
-      Same as push local i but use @ARG
-      pop argument i
-      Same as pop local i but use @ARG
-
-      push this i
-      Same as push local i but use @THIS
-      pop this i
-      Same as pop local i but use @THIS
-      push that i
-      Same as push local i but use @THAT
-      pop that i
-      Same as pop local i but use @THAT
-
-      // push static i
-      // RAM[SP] = Foo.i
-      @Foo.i
-      D=M
-      @SP
-      A=M
-      M=D
-      // SP++
-      @SP
-      M=M+1
-
-      pop static i
-      // SP--
-      @SP
-      AM=M-1
-      // Foo.i = RAM[SP]
-      D=M
-      @Foo.i
-      M=D
-
-      push pointer 0
-      // RAM[SP] = THIS
-      @THIS
-      D=M
-      @SP
-      A=M
-      M=D
-      // SP++
-      @SP
-      M=M+1
-
-      pop pointer 0
-      // SP--
-      @SP
-      M=M-1
-      // THIS = RAM[SP]
-      A=M
-      D=M
-      @THIS
-      M=D
-
-      push pointer 1
-      Same as push pointer 0 but with THAT
-      pop pointer 1
-      Same as pop pointer 0 but with THAT
-
-      push temp i
-      Same as push local i but use @5
-      pop temp i
-      Same as pop local i but use @5
-    */
+  public writePushPop(
+    command: "C_PUSH" | "C_POP",
+    segment: MemorySegment,
+    index: number
+  ): void {
+    let translation;
+    if (segment === "argument") {
+      translation =
+        command === "C_POP"
+          ? writePopArgument({ i: index })
+          : writePushArgument({ i: index });
+    } else if (segment === "constant") {
+      translation = writePushConstant({ i: index });
+    } else if (segment === "local") {
+      translation =
+        command === "C_POP"
+          ? writePopLocal({ i: index })
+          : writePushLocal({ i: index });
+    } else if (segment === "pointer") {
+      if (command === "C_POP" && index === 0) {
+        translation = writePopPointer0();
+      } else if (command === "C_POP" && index === 1) {
+        translation = writePopPointer1();
+      } else if (command === "C_PUSH" && index === 0) {
+        translation = writePushPointer0();
+      } else if (command === "C_PUSH" && index === 1) {
+        translation = writePushPointer1();
+      }
+    } else if (segment === "static") {
+      translation =
+        command === "C_POP"
+          ? writePopStatic({ i: index })
+          : writePushStatic({ i: index });
+    } else if (segment === "temp") {
+      translation =
+        command === "C_POP"
+          ? writePopTemp({ i: index })
+          : writePushTemp({ i: index });
+    } else if (segment === "that") {
+      translation =
+        command === "C_POP"
+          ? writePopThat({ i: index })
+          : writePushThat({ i: index });
+    } else if (segment === "this") {
+      translation =
+        command === "C_POP"
+          ? writePopThis({ i: index })
+          : writePushThis({ i: index });
+    }
+    if (translation) {
+      this.fileWriter?.write(translation);
+    }
   }
   public close() {
     this.fileWriter?.end();
   }
 }
 
-const file = Bun.file('./package.json');
-const stream = file.stream();
-const reader = stream.getReader();
-const {value: chunk, done} = await reader.read();
-const textDecoder = new TextDecoder();
-const stringChunk = textDecoder.decode(chunk);
-console.log(stringChunk);
+/*
+  ============================================================================
+  MAIN
+  ============================================================================
+*/
+
+async function compile(src: BunFile, dest: BunFile) {
+  const parser = await Parser.create(src);
+  const codeWriter = await CodeWriter.create(dest);
+  while (await parser.hasMoreCommands()) {
+    await parser.advance();
+  
+    const commandType = parser.commandType();
+    if (commandType === "C_ARITHMETIC") {
+      const arg1 = parser.arg1();
+      if (arg1 && isArithmeticCommand(arg1)) {
+        codeWriter.writeArithmetic(arg1);
+      }
+    } else if (commandType === "C_PUSH" || commandType === "C_POP") {
+      const arg1 = parser.arg1();
+      const arg2 = parser.arg2();
+      if (arg1 && isMemorySegment(arg1) && typeof arg2 !== 'undefined') {
+        codeWriter.writePushPop(commandType, arg1, arg2);
+      }
+    }
+  }
+}
+const SimpleAddVm = Bun.file('./tests/SimpleAdd/SimpleAdd.vm');
+const SimpleAddAsm = Bun.file("./output/SimpleAdd.asm");
+const BasicTestVm = Bun.file('./tests/BasicTest/BasicTest.vm');
+const BasicTestAsm = Bun.file("./output/BasicTest.asm");
+const PointerTestVm = Bun.file('./tests/PointerTest/PointerTest.vm');
+const PointerTestAsm = Bun.file("./output/PointerTest.asm");
+const StackTestVm = Bun.file('./tests/StackTest/StackTest.vm');
+const StackTestAsm = Bun.file("./output/StackTest.asm");
+const StaticTestVm = Bun.file('./tests/StaticTest/StaticTest.vm');
+const StaticTestAsm = Bun.file("./output/StaticTest.asm");
+
+await compile(SimpleAddVm, SimpleAddAsm);
+await compile(BasicTestVm, BasicTestAsm);
+await compile(PointerTestVm, PointerTestAsm);
+await compile(StackTestVm, StackTestAsm);
+await compile(StaticTestVm, StaticTestAsm);
+console.log("Finished translating all VM files in tests folder");
